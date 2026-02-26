9d711847d29bba6c52510ed3d5dc4a25ecd91ac6
Tran Dieu <tracpham2210@gmail.com>
2026-02-27 00:43:45 +0700

    Fix cross-domain cookie issue: change sameSite to 'none' for production

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -20,8 +20,8 @@ export const setJwtAuthCookie = ({ res, userId }: Cookie) => {
    return res.cookie("accessToken", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      secure: Env.NODE_ENV === "production" ? true : false,
-      sameSite: Env.NODE_ENV === "production" ? "strict" : "lax",
+      secure: Env.NODE_ENV === "production",
+      sameSite: Env.NODE_ENV === "production" ? "none" : "lax",
    })
 };
 
